817fb20d04d24ad24c09167f85a8df3a4b90699b
Everton93 <evesilvacosta@gmail.com>
2021-08-21 18:22:45 -0300

    Finalização do projeto Parte I, Injeção de depencia

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -8,11 +8,23 @@ import { UsersRepository } from "@modules/users/infrastructure/typeorm/repositor
 import IUserTokenRepository from "@modules/users/Domain/Repository/IUserTokenRepository";
 import { UsersTokenRepository } from "@modules/users/infrastructure/typeorm/repositories/UsersTokenRepository";
 import IGuestRepository from "@modules/guest/Domain/Repository/IGuestRepository";
-import { GuestRepository } from "@modules/guest/infrastructure/typeorm/Repository/GuestRepository";
+import {GuestRepository}  from "@modules/guest/infrastructure/typeorm/Repository/GuestRepository";
+import IRoomsRepository from "@modules/room/Domain/Repository/IRoomsRepository";
+import RoomsRepository from "@modules/room/infrastructure/typeorm/Repository/RoomsRepository";
+import IReserveRepository from "@modules/room/Domain/Repository/IReserveRepository";
+import ReserveRepository from "@modules/room/infrastructure/typeorm/Repository/ReserveRepository"
+import IServiceRoomRepository from "@modules/room/Domain/Repository/IServiceRoomRepository";
+import ServiceRoomRepository from "@modules/room/infrastructure/typeorm/Repository/ServiceRoomRepository"
+import ICheckOutRepository from "@modules/room/Domain/Repository/ICheckOutRepository";
+import CheckOutRepository from "@modules/room/infrastructure/typeorm/Repository/CheckOutRepository"
+
 
 container.registerSingleton<ISuplierRepository>('SuplierRepository', SuplierRepository);
 container.registerSingleton<IProductRepository>('ProductsRepository', ProductsRepository);
 container.registerSingleton<IUserRepository>('UsersRepository', UsersRepository);
 container.registerSingleton<IUserTokenRepository>('UsersTokenRepository', UsersTokenRepository);
 container.registerSingleton<IGuestRepository>('GuestRepository', GuestRepository);
-
+container.registerSingleton<IRoomsRepository>('RoomsRepository', RoomsRepository);
+container.registerSingleton<IReserveRepository>('ReserveRepository', ReserveRepository);
+container.registerSingleton<IServiceRoomRepository>('ServiceRoomRepository', ServiceRoomRepository);
+container.registerSingleton<ICheckOutRepository>('CheckOutRepository', CheckOutRepository);
